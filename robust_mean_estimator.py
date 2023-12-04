6d11e795fef635c23f29e3b894d95adbfd44b3f8
Hammad Izhar <hammad_izhar@brown.edu>
2023-12-04 03:00:22 -0500

    feat: robust covariance, not very efficient

--- robust_mean_estimator.py
+++ robust_mean_estimator.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+from tqdm import tqdm
 import numpy as np
 
 
@@ -21,15 +22,39 @@ class RobustMeanEstimator(ABC):
         assert len(data.shape) == 2, "The data must be a matrix."
         assert 0 <= epsilon <= 1, "Epsilon must be in [0, 1]."
 
-        self.n, self.d = data.shape
-        self.estimation = self._estimate(data, epsilon)
-        assert self.estimation.shape == (self.d,), (
+        n, d = data.shape
+        estimation = self._estimate(data, epsilon)
+        assert estimation.shape == (d,), (
             "The estimated mean must be a vector: "
-            + str(self.estimation.shape)
+            + str(estimation.shape)
             + " vs "
-            + str(self.d)
+            + str(d)
         )
-        return self.estimation
+        return estimation
+
+    def _restructure_covariance_estimate_rows(self, samples):
+        assert len(samples.shape) == 3, "The samples must be a 3D tensor."
+        n, d1, d2 = samples.shape
+        assert d1 == d2, "The samples must be square matrices."
+
+        return samples.transpose(1, 0, 2).reshape(d1, n, d1)
+
+
+    def estimate_covariance(self, data: np.ndarray, epsilon: float) -> np.ndarray:
+        assert len(data.shape) == 2, "The data must be a matrix."
+        assert 0 <= epsilon <= 1, "Epsilon must be in [0, 1]."
+
+        n, d = data.shape
+
+        centered_data = data - self._estimate(data, epsilon)
+        cov_samples = np.array([np.outer(centered_data[i], centered_data[i]) for i in tqdm(range(n))])
+
+        print("transposing")
+        restructured_cov_samples = self._restructure_covariance_estimate_rows(cov_samples)
+
+        print("final computation")
+        return np.array([self._estimate(sample, epsilon) for sample in tqdm(restructured_cov_samples)])
+
 
     def cleanup(self):
         """
